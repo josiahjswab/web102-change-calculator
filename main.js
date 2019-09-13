1af745862e2aaad7f35a8ce0e392e39ed073af96
Josiah Swab <josiahjswab@gmail.com>
2019-09-12 17:06:57 -0700

    added the function calculate change using modulo

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 // .----------------.  .----------------.  .----------------. 
 // | .--------------. || .--------------. || .--------------. |
 // | |  _______     | || |      __      | || |  ________    | |
@@ -10,10 +9,32 @@
 // | |              | || |              | || |              | |
 // | '--------------' || '--------------' || '--------------' |
 //  '----------------'  '----------------'  '----------------' 
-function clickEventHandler(){
-    var charge = parseInt(document.getElementById('amount-due').value);
-    var tender = parseInt(document.getElementById('amount-received').value);
-    var change = (tender-charge).toFixed(2);
-    document.getElementById('output').innerText = change;//this will not be in final product.
+function clickEventHandler() {
+    var charge = document.getElementById('amount-due').value;
+    var tender = document.getElementById('amount-received').value;
+    var change = (tender - charge).toFixed(2);
+    document.getElementById('output').innerText = change; //this will not be in final product.
+    return change;
 }
 
+function calculateChange() {
+
+    var dollars = Math.floor(clickEventHandler());
+    var change = change%1.00;
+    var quarters = Math.floor(change/.25);
+    var change = change%.25;
+    var dimes = Math.floor(change/.10);
+    var change = change%.10;
+    var nickels = Math.floor(change/.05);
+    var change = change%.05;
+    var pennies = Math.floor(change/.01);
+    var change = change*100;
+
+    document.getElementById('dollars-output').innerHTML = 'Dollars:' + dollars;
+    document.getElementById('quarters-output').innerHTML = quarters;
+    document.getElementById('dimes-output').innerHTML = 'Dimes:' + dimes;
+    document.getElementById('nickels-output').innerHTML = 'Nickels:' + nickels;
+    document.getElementById('pennies-output').innertext = 'Pennies:' + pennies;
+
+
+}
